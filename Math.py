69d5bd33535f7999454bb5c3edd6c5fca347ed0e
Zaheer_Ahmad <zaheerahmadlangrial92@gmail.com>
2024-01-05 18:36:15 +0500

    Learn Math, math in Python

---
+++ Math.py
@@ -0,0 +1,26 @@
+import Math
+import math
+
+
+
+x = min(3, 4, 5, 3,22,3)
+
+print('The small letter is : ' ,x)
+
+
+x = max(3, 44, 5,3,5)
+
+print('The Largest VAlues is : ' , x)
+
+
+        # the power Method
+
+x = pow(5 , 2) 
+print(x)
+
+
+    # Math.sqrt() 
+
+x = math.sqrt(64)
+
+print(int(x))
