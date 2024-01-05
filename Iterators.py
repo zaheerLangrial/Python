05a18b524b35f8b9cc88307bcdd9cd83853eb378
Zaheer_Ahmad <zaheerahmadlangrial92@gmail.com>
2024-01-05 16:59:28 +0500

    Learn Polymorphism in Python

---
+++ Iterators.py
@@ -0,0 +1,30 @@
+
+    # Python Iterators
+
+myTuple = ('Banana' , 'Apple' , 'Mango' , 'Cheery')
+
+myIt = iter(myTuple)
+
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+
+
+myTuple = 'Banana'
+myIt = iter(myTuple)
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+
+
+        # Iterators me hmay 2 methods milty hain jo mil k km karty hain ik to hy hamra iter() the 2nd is next()
+
+
+
+    
+
+
